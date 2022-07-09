0c310b25e723ed6facb045fc8b5cbffa91193496
Amaan <73187712+amaan14999@users.noreply.github.com>
2022-07-09 17:24:58 +0530

    Update 125. Valid Palindrome.java

--- Strings/125. Valid Palindrome.java
+++ Strings/125. Valid Palindrome.java
@@ -17,3 +17,36 @@ class Solution {
     }
 }
 //18 lines, o(n), 798ms
+
+class Solution {
+    public boolean isPalindrome(String s) {
+        int start=0;
+        int end=s.length()-1;
+        while(start<end)
+        {
+            char head=s.charAt(start);
+            char tail=s.charAt(end);
+            if(!Character.isLetterOrDigit(head))
+            {
+                start++;
+            }
+            else if(!Character.isLetterOrDigit(tail))
+            {
+                end--;
+            }
+            else 
+            {
+                if(Character.toLowerCase(head)!=Character.toLowerCase(tail))
+                {
+                    
+                    return false;
+                }
+                start++;
+                end--;
+            }
+            
+        }
+        return true;
+    }
+}
+//20 lines, O(n), 6ms
