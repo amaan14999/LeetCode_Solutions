1ad2e568ccd8ce072ae5042798bdd75fb2c5d75a
Amaan <73187712+amaan14999@users.noreply.github.com>
2022-07-10 02:10:50 +0530

    Update 438. Find All Anagrams in a String.java

--- Strings/438. Find All Anagrams in a String.java
+++ Strings/438. Find All Anagrams in a String.java
@@ -26,3 +26,38 @@ class Solution {
     }
 }
 //27 lines, sliding window noob apporach 1550ms
+
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        int[] map = new int[26];
+        List<Integer> result = new ArrayList<>();
+        
+        for(int i=0;i<p.length();i++){
+            map[p.charAt(i) - 'a']++;
+        }
+    
+        int windowStart = 0;
+        int windowEnd = 0;
+        while(windowEnd<s.length()){
+		// valid anagram
+            if(map[s.charAt(windowEnd) - 'a'] > 0){
+                map[s.charAt(windowEnd++) - 'a']--;
+			// window size equal to size of P
+                if(windowEnd-windowStart ==  p.length()){                    
+                    result.add(windowStart);
+                }
+            }
+			// window is of size 0
+            else if(windowStart == windowEnd){
+                windowStart ++;
+                windowEnd ++;
+            }
+			// decrease window size
+            else{
+                map[s.charAt(windowStart++) - 'a']++;
+            }      
+        }
+        return result;
+    }
+}
+//33 lines, sliding window, O(S),7ms
