9a52c5f4625cf6f33d917b2bb234a098444696c4
Amaan <73187712+amaan14999@users.noreply.github.com>
2022-07-10 02:46:43 +0530

    Update 287. Find the Duplicate Number.java

--- Array/287. Find the Duplicate Number.java
+++ Array/287. Find the Duplicate Number.java
@@ -12,3 +12,20 @@ class Solution {
     }
 }
 //13 lines, sorting apporach, Time Complexity: O(nlogn), Space Complexity: O(logn), 41ms
+
+
+class Solution {
+    public int findDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
+            if (!set.add(nums[i])) {
+                return nums[i];
+            }
+        }
+
+        return len;
+    }
+}
+
+//13 lines, hashset, Time Complexity: O(n), Space Complexity: O(n), 54ms
