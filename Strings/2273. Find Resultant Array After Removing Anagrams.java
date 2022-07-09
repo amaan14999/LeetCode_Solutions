2f24de2824fd31ed81c40445319ed6a01d82b097
Amaan <73187712+amaan14999@users.noreply.github.com>
2022-07-09 21:04:59 +0530

    Update 2273. Find Resultant Array After Removing Anagrams.java

--- Strings/2273. Find Resultant Array After Removing Anagrams.java
+++ Strings/2273. Find Resultant Array After Removing Anagrams.java
@@ -19,3 +19,36 @@ class Solution {
     }
 }
 //21 lines, o(n), 4ms
+
+
+class Solution {
+    public List<String> removeAnagrams(String[] words) {
+        Stack<String> stack=new Stack<>();
+        for(int i=words.length-1;i>=0;i--)
+        {
+            String s=words[i];
+            if(!stack.isEmpty()&& isanagram(stack.peek(),s)==true)
+            {
+                stack.pop();
+            }
+            stack.push(s);
+        }
+        ArrayList<String> list=new ArrayList<>();
+        while(!stack.isEmpty())
+        {
+            list.add(stack.pop());
+        }
+        return list;
+    }
+    
+    public boolean isanagram(String p, String q)
+    {
+        int[] ans=new int[26];
+        for(int i=0;i<p.length();i++) ans[p.charAt(i)-'a']++;
+        for(int i=0;i<q.length();i++) ans[q.charAt(i)-'a']--;
+        for(int i:ans) if(i!=0) return false;
+        return true;
+    }
+}
+
+//29 lines, O(m+n), 7ms
