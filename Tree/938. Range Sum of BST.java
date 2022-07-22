c67d5863f3d167151bb3ada269a13344db728654
Amaan <73187712+amaan14999@users.noreply.github.com>
2022-07-22 15:04:41 +0530

    Update 938. Range Sum of BST.java

--- Tree/938. Range Sum of BST.java
+++ Tree/938. Range Sum of BST.java
@@ -20,4 +20,29 @@ class Solution {
         
     }
 }
-//20 lines, O(n) time
+//20 lines, O(n) time, 1ms
+
+//OPTIMISED APPROACH
+class Solution {
+    int sum=0;
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        range(root,low,high);
+        return sum;
+    }
+    void range(TreeNode root,int low, int high)
+    {
+        if(root==null)
+            return;
+        
+        if(root.val>=low&&root.val<=high)
+        sum+=root.val;
+        
+        if(root.val>low)
+        range(root.left,low,high);
+        
+        if(root.val<high)
+        range(root.right,low,high);
+        
+    }
+}
+//20 lines, O(n) time, 0ms
